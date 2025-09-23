1716c955c22522f40d77390eba1ef7b5d376272f
mutha <mutha@PREMSAI-LAPPY>
2025-09-23 20:48:07 +0530

    TL - project directory created.

---
+++ maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/App.java
@@ -0,0 +1,13 @@
+package com.premit.tutorial.maven_project_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-project-git/src/test/java/com/premit/tutorial/maven_project_git/AppTest.java
@@ -0,0 +1,5 @@
+package com.premit.tutorial.maven_project_git;
+
+public class AppTest {
+
+}
