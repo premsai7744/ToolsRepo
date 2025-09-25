e58c5ad236e24ffa086be1c0d0483a342328d76f
mutha <mutha@PREMSAI-LAPPY>
2025-09-25 11:22:16 +0530

    DEV1: added systrace statement in B.java file in arithmetic(-,-);

--- maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/B.java
+++ maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/B.java
@@ -1,6 +1,7 @@
 package com.premit.tutorial.maven_project_git;
 public class B extends Exception {
 	public int arithmetic(int a,int b) {
+		System.out.println("===Arithmetic===");
 			return a+b;
 		}
 }
