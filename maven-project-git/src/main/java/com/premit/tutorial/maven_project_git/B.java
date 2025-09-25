87288faab98c319d40a4fb108e57d1aa58d212d2
mutha <mutha@PREMSAI-LAPPY>
2025-09-25 14:43:58 +0530

    TL: systrace t1's code

--- maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/B.java
+++ maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/B.java
@@ -1,7 +1,7 @@
 package com.premit.tutorial.maven_project_git;
 public class B extends Exception {
 	public int arithmetic(int a,int b) {
-
+		System.out.println("Tl's Code");
 		System.out.println("***Addition***");
 
 		System.out.println("===Arithmetic===");
