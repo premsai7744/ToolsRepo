91bb3f85418671bc3b0032745c35f3e56000b05f
mutha <mutha@kubernetes.docker.internal>
2025-09-25 14:43:17 +0530

    DEV1: systrace:dev1 code

--- maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/B.java
+++ maven-project-git/src/main/java/com/premit/tutorial/maven_project_git/B.java
@@ -1,7 +1,7 @@
 package com.premit.tutorial.maven_project_git;
 public class B extends Exception {
 	public int arithmetic(int a,int b) {
-
+		System.out.println("DEV1 CODE");
 		System.out.println("***Addition***");
 
 		System.out.println("===Arithmetic===");
