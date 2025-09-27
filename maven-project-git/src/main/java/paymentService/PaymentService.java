c6da35f8e0ec818e50f6f043111206d6d89b429d
mutha <mutha@PREMSAI-LAPPY>
2025-09-27 14:40:55 +0530

    DEV1: added payment service in payment branch

---
+++ maven-project-git/src/main/java/paymentService/PaymentService.java
@@ -0,0 +1,10 @@
+package paymentService;
+
+public class PaymentService {
+	public String doUpiPayment() {
+		return "upi payment is done..";
+	}
+	public String doCardPayment() {
+		return "card payment is done..";
+	}
+}
